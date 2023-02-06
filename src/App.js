8c0763179262ebfc6d8aa0ecf4ef737cbac1909b
NewbieRitesh <riteshsharma34567@gmail.com>
2023-02-06 13:11:27 +0530

    dark mode enabled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Workspace from "./components/Workspace";
-
 function App() {
+  const [themeMode, setthemeMode] = useState({
+    color: 'black',
+    backgroundColor: 'white'
+  })
+  const [themeBtnText, setThemeBtnText] = useState('Enable Dark Mode')
+
+  const theme = () => {
+    if (themeMode.color === 'black') {
+      setthemeMode({
+        color: 'white',
+        backgroundColor: 'black'
+      })
+      setThemeBtnText('Enable Light Mode')
+
+    }
+    else {
+      setthemeMode({
+        color: 'black',
+        backgroundColor: 'white'
+      })
+      setThemeBtnText('Enable Dark Mode')
+    }
+  }
+
   return (
-    <>
-      <Navbar title="TextUtils" />
+    <><div style={themeMode}>
+      <Navbar title="TextUtils"/>
+      <button className="btn btn-primary" onClick={theme}>{themeBtnText}</button>
       <div className="container">
         <Workspace />
       </div>
-    </>
+      </div></>
   );
 }
-
 export default App;
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -7,10 +7,14 @@ export default function Workspace() {
     const lowercaseFunc = () => {
         setData(data.toLowerCase())
     }
+    const clearTextFunc = () => {
+        setData('')
+    }
     const [data, setData] = useState('')
     const handleChange = (event) => {
         setData(event.target.value);
     }
+
     return (
         <>
             <div className="form my-3">
@@ -18,6 +22,7 @@ export default function Workspace() {
                 <textarea className="form-control my-3" value={data} onChange={handleChange} placeholder="Enter Text here" rows="8" id="dataofuser"></textarea>
                 <button className="btn btn-primary mx-1" onClick={uppercaseFunc}>Change to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={lowercaseFunc}>Change to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={clearTextFunc}>Clear Text</button>
             </div>
             <div>
                 <h1>Text Summary</h1>
