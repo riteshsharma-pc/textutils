182bcae6134afbe01d81afce953aeaca196decb0
NewbieRitesh <riteshsharma34567@gmail.com>
2023-02-08 10:49:36 +0530

    toggle icon dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import Navbar from "./components/Navbar";
 import Workspace from "./components/Workspace";
 import { useState } from 'react'
+import './App.css'
 function App() {
   const [mode, setMode] = useState('light')
   const [theme, setTheme] = useState()
+  const [filter, setFilter] = useState()
   const switchDarkMode = () => {
     if (mode === 'light') {
       setMode('dark')
       themeMode(mode)
+      invertStyle(mode)
     }
     else {
       setMode('light')
       themeMode(mode)
+      invertStyle(mode)
+    }
+  }
+  const invertStyle = () =>{
+    if(mode==='light'){
+      setFilter('filter')
+    }
+    else{
+      setFilter('nofilter')
     }
   }
   const themeMode = () => {
@@ -31,7 +43,7 @@ function App() {
 
   return (
     <>
-      <Navbar title="TextUtils" switchDarkMode={switchDarkMode} mode={mode} />
+      <Navbar title="TextUtils" switchDarkMode={switchDarkMode} mode={mode} filter={filter} />
       <Workspace theme={theme} />
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className={`navbar-toggler-icon ${props.filter}`}></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
