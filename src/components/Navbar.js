43f9ec53ed21c24c063bd79010b77cd62811de06
NewbieRitesh <riteshsharma34567@gmail.com>
2023-02-11 00:30:00 +0530

    improvement in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ export default function Navbar(props) {
     return (
         <>
             
-            <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary fixed-top" data-bs-theme={props.mode}>
+            <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary" data-bs-theme={props.mode}>
                 <div className="container-fluid">
                     <Link className="navbar-brand" to='/'>{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
